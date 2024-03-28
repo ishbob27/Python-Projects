bfd8783fd6dc9fe2e88e02db0628eeca5528a78d
Ishaaq Ismail <bobt4648@gmail.com>
2024-03-28 00:59:10 -0700

    commit

--- basicpass.py
+++ basicpass.py
@@ -7,44 +7,42 @@ import random
 print()
 print("Welcome to the Password Generator!\n") 
 
-# # letters 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-# # User input for how many letters they want to use
-nr_letters= int(input("How many letters would you like in your password? "))  
-
 # function that generates random letters based on the users input
-def random_letters():   
+def random_letters():  
+    # letters 
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    # User input for how many letters they want to use
+    nr_letters= int(input("How many letters would you like in your password? "))  
     l = ''
     for i in range(nr_letters):
         random_char = random.choice(letters) 
         l += random_char 
     return l 
 
-# symbols 
-symbols = ['!', '#', '$', '%', '&', '@', '-', '*', '+'] 
-
-# User input
-nr_symbols = int(input(f"How many symbols would you like? "))  
 
 # funtion that generates random symbols from the list of symbols provided 
-
 def random_sym(): 
+    # symbols 
+    symbols = ['!', '#', '$', '%', '&', '@', '-', '*', '+']  
+    
+    # User input
+    nr_symbols = int(input(f"How many symbols would you like? ")) 
+    print()
     s = '' 
     for i in range(nr_symbols): 
         random_symbol = random.choice(symbols) 
         s += random_symbol 
     return s 
 
-#numbers
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-# User input
-nr_numbers = int(input(f"How many numbers would you like? ")) 
-print()
-
 # function that generates a list of random numbers based on the users input 
-def random_int(): 
+def random_int():   
+    
+    # numbers 
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+
+    # User input 
+    nr_numbers = int(input(f"How many numbers would you like? "))  
     n = ''
     for i in range(nr_numbers): 
         random_number = random.choice(numbers) 
